66bad4c47f47e34b7ef031eaf68ff8724bb6e689
Pianonic <79938743+Pianonic@users.noreply.github.com>
2022-10-30 01:32:54 +0200

    Open Code by PianoNic

---
+++ HandySelecter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HandySelecter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklarierung und Initalisierung der Marken.
+            string[] Marken = {"Apple", "Samsung", "Huawei", "Oppo", "Xiaomi", "BlackBerry" }; 
+            
+            int AnzahlMaken = Marken.Length; //Auslesen wie viele marken es gibt. In diesem Beispiel 6.
+
+            Random zufällig = new Random(); //Deklarierung vom Typ Random.
+            int num = zufällig.Next(AnzahlMaken); //Generieren einer zahl zwischen 0 und 5.
+
+            Console.WriteLine(Marken[num]); //Passend zur generierten Zahl wird nun eine Marke ausgegeben.
+        }
+    }
+}
